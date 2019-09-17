8baea0b5e5217771056a4b3614a6439d6e1e8568
Bryan Cross <bryancross@github.com>
2019-09-17 10:28:04 -0700

    adding adrians code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumControl.java
@@ -0,0 +1,89 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Color;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorColor;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@TeleOp(name="Meccy McMecherson")
+public class MecanumControl extends OpMode {
+
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor leftRear;
+    DcMotor rightRear;
+    DcMotor arm;
+    Servo marker;
+
+    ColorSensor color;
+    DistanceSensor dist;
+
+    boolean brakeToggle = false;
+
+    @Override
+    public void init() {
+
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        leftRear = hardwareMap.dcMotor.get("leftRear");
+        rightRear = hardwareMap.dcMotor.get("rightRear");
+        arm = hardwareMap.dcMotor.get("arm");
+        marker = hardwareMap.servo.get("marker");
+
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        color = hardwareMap.colorSensor.get("color");
+        dist = hardwareMap.get(DistanceSensor.class, "dist");
+
+        // We show the log in oldest-to-newest order, as that's better for poetry
+        telemetry.log().setDisplayOrder(Telemetry.Log.DisplayOrder.OLDEST_FIRST);
+        // We can control the number of lines shown in the log
+        telemetry.log().setCapacity(7);
+
+    }
+
+    @Override
+    public void loop() {
+
+        telemetry.addData("Red", color.red());
+        telemetry.addData("Green", color.green());
+        telemetry.addData("Blue", color.blue());
+        telemetry.addData("Dist", dist.getDistance(DistanceUnit.CM));
+        telemetry.update();
+
+        double forward = -gamepad1.right_stick_y;
+        double turn = gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+
+        leftFront.setPower( turn - forward - strafe);
+        rightFront.setPower(turn + forward - strafe);
+        leftRear.setPower(  turn - forward + strafe);
+        rightRear.setPower( turn + forward + strafe);
+
+        arm.setPower(gamepad2.left_stick_y);
+
+        /* NOTES
+
+         */
+
+    }
+
+}
